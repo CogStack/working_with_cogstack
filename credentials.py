70167cf2d107c82e8bf4f58cd3370f0291389bc4
Anthony Shek <antsh3k@users.noreply.github.com>
2022-06-23 19:42:55 +0100

    add credentials space to save login credentials

---
+++ credentials.py
@@ -0,0 +1,9 @@
+# Any questions on what these details are please contact your local CogStack administrator.
+
+hosts = []  # This is a list of your CogStack ElasticSearch instances.
+
+# These are your login details (either via http_auth or API)
+username = ""
+password = ""
+api_username = ""
+api_password = ""
