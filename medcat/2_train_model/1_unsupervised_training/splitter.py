9b4ca49c8fc47e166ef2a3b00a88f3564b672ea8
mart-r <mart.ratas@gmail.com>
2022-11-25 15:17:45 +0000

    CU-2uhvthh Add unsupervised training procedure as I used it for UMLS

---
+++ medcat/2_train_model/1_unsupervised_training/splitter.py
@@ -0,0 +1,167 @@
+
+import re
+
+import pydantic
+
+
+class SplitIdentifier:
+    start_line_start_pattern: re.Pattern = re.compile('(\d+),(\d+),')
+
+    def is_first_line(self, line: str) -> bool:
+        """Check if the line in question is a suitable first line for an entry.
+
+        The schema:
+        "subject_id","hadm_id","chartdate","charttime","text","category","description"
+
+        However, "text" is often multiline.
+        So an example first line could be:
+        24776,139951,"2154-11-15 00:00:00","2154-11-15 17:48:00","HPI:
+        That is, "subject_id","hadm_id","chartdate","charttime" and the start of "text"
+
+        So currently, I am checking that the line:
+        a) Starts with 2 integers separated by a comma
+        b) Has an uneven number of quotation marks (i.e ends with an open quote)
+        c) The number of quotes is greater than 4
+
+        Args:
+            line (str): The line in question
+
+        Returns:
+            bool: True if it's a suitable first line
+        """
+        # check if starts with 2 integers separated by comma
+        if not self.start_line_start_pattern.match(line):
+            return False
+        nr_of_quotes = line.count('"')
+        return (nr_of_quotes // 2) != (nr_of_quotes / 2) and nr_of_quotes > 4
+
+    def is_last_line(self, line: str) -> bool:
+        """Check if the lin in question is a suitable last line for an entry.
+
+        The schema:
+        "subject_id","hadm_id","chartdate","charttime","text","category","description"
+
+        However, "text" is often multiline.
+        So an example last line could be:
+        ","Physician ","Physician Resident Progress Note"
+        That is, the end of "text" and then "category","description"
+
+        So currently I am checking that the line:
+        a) Has an uneven number of quotation marks (i.e starts with an open quote)
+        b) Number of quotes is greater than 4
+
+        Args:
+            line (str): The line in question
+
+        Returns:
+            bool: True if it's a suitable last line
+        """
+        nr_of_quotes = line.count('"')
+        return (nr_of_quotes // 2) != (nr_of_quotes / 2) and nr_of_quotes > 4
+
+
+class SplitOptions(pydantic.BaseModel):
+    lines_at_a_time: int
+    out_file_format: str
+    header_length: int = 1
+
+
+class SplitBuffer:
+
+    def __init__(self, file_nr: int, opts: SplitOptions, split_identifier: SplitIdentifier, header: str) -> None:
+        self.file_nr = file_nr
+        self.opts = opts
+        self.split_identifier = split_identifier
+        self.lines: list = [header]
+        self.prev_line_is_last = False
+        self._is_done = False
+
+    def save(self) -> None:
+        file_name = self.opts.out_file_format % self.file_nr
+        print('Saving', len(self.lines), 'to file nr',
+              self.file_nr, ':', file_name)
+        with open(file_name, 'w') as fw:
+            fw.writelines(self.lines)
+
+    def process_or_write(self, line_nr: int, line: str) -> 'SplitBuffer':
+        """Process line and write if needed.
+
+        If processing a line results in saving the data into a file, a new SplitBuffer is returned.
+        This new instance will have the first line added to it already.
+        If processing did not result in saving the data, the same instance is returned.
+
+        Args:
+            line_nr (int): The number of the line in the original
+            line (str): The line contents
+
+        Returns:
+            SplitBuffer: Returns an instance of the buffer that should be used
+        """
+        if self._is_done:
+            raise ValueError('Cannot reuse a SplitBuffer - create a new one')
+        # line = line.replace('\n', '')
+        has_passed_req_line = line_nr >= self.opts.lines_at_a_time * self.file_nr
+        cur_line_is_last = self.split_identifier.is_last_line(line)
+        cur_line_is_first = self.split_identifier.is_first_line(line)
+        if has_passed_req_line and self.prev_line_is_last and cur_line_is_first:
+            print('Currently at line', line_nr)
+            self.save()
+            # print('Saving', len(self.lines), 'up until', line_nr, 'to file number', self.file_nr, ':', out_file)
+            # print('PREV line:\n', self.lines[-1])
+            # print('NEW line:\n', line)
+            self._is_done = True
+            buffer = SplitBuffer(
+                self.file_nr + 1, self.opts, self.split_identifier, header=self.lines[0])
+            return buffer.process_or_write(line_nr, line)
+        if cur_line_is_last:
+            self.prev_line_is_last = cur_line_is_last
+        self.lines.append(line)
+        return self
+
+
+class Splitter:
+
+    def __init__(self, opts: SplitOptions, split_identifier: SplitIdentifier) -> None:
+        self.opts = opts
+        self.split_identifier = split_identifier
+
+    def split(self, in_file: str):
+        with open(in_file, 'r') as f:
+            buffer = None
+            for line_nr, line in enumerate(f):
+                if buffer is None:  # for the first line, just consider the header
+                    buffer = SplitBuffer(
+                        1, self.opts, self.split_identifier, header=line)
+                    continue
+                buffer = buffer.process_or_write(line_nr, line)
+        if len(buffer.lines) > 1: # if there's more than just a header
+            buffer.save() # saver remaining
+
+
+def split_file(in_file: str, nr_of_lines: int, out_file_format: str) -> None:
+    opts = SplitOptions(lines_at_a_time=nr_of_lines,
+                        out_file_format=out_file_format)
+    split_identifier = SplitIdentifier()
+    splitter = Splitter(opts, split_identifier)
+    splitter.split(in_file)
+
+
+if __name__ == '__main__':
+    import sys
+    if len(sys.argv) < 3:
+        print('Need to specify in original file name and target file format')
+        sys.exit(2)
+    orig_file = sys.argv[1]
+    target_format = sys.argv[2]
+    if '%d' not in target_format:
+        print('Target format needs to contain "%d" for including number in the file names')
+        sys.exit(2)
+    nr_of_lines = 300000
+    if len(sys.argv) > 3:
+        try:
+            nr_of_lines = int(sys.argv[3])
+        except ValueError:
+            print(
+                'Third argument needs to be numeric (for the number of lines per each split)')
+            sys.exit(2)
+    split_file(orig_file, nr_of_lines, target_format)
---
+++ medcat/2_train_model/1_unsupervised_training/unsupervised_training.py
@@ -0,0 +1,59 @@
+from medcat.cat import CAT
+import pandas as pd
+import os
+import logging
+
+# python medcat/2_train_model/1_unsupervised_training/splitter.py
+# medcat/2_train_model/1_unsupervised_training/all_notes.csv medcat/2_train_model/1_unsupervised_training/split_notes_5M_%d.csv 5000000
+#
+
+all_notes_file = 'all_notes.csv' # CHANGE AS NEEDED
+
+# in my case, I needed to split the notes into parts. Otherwise, the work just crashed at some point
+# I chose to split into 19 parts, around 5000000 lines at a time.
+split_format = 'split_notes_5M_%d.csv'
+nr_of_lines = 5000000
+
+from splitter import split_file
+
+if not os.path.exists(split_format%1):
+    print(f'\n\nSplitting file into {nr_of_lines} line at a time. This will probably take some time\n\n')
+    split_file(all_notes_file, nr_of_lines, split_format)
+    print('\n\nDone with the split!\n\n')
+else:
+    print('\n\nNB!Expecting the split files to already exist\n\n')
+
+data_dir = '.' # CHANGE AS NEEDED
+
+model_dir = '../../../data/medcat_models/modelpack'
+
+modelpack = 'umls_model2_zip_0d4ccc7b9ae1ecd2.zip' # CHANGE AS NEEDED
+model_pack_path = os.path.join(model_dir, modelpack)
+
+output_modelpack = 'umls_self_train_model'  # Save name for new model
+
+# Load modelpack
+print('Loading modelpack')
+cat = CAT.load_model_pack(model_pack_path)
+cat.log.addHandler(logging.StreamHandler()) # add console output
+
+print('STATS:')
+cat.cdb.print_stats()
+
+# CHANGE AS NEEDED - if the number of spligt files is different
+all_data_files = [f'split_notes_5M_{nr}.csv' for nr in range(1, 20)]  # file containing training material.
+for i, data_file in enumerate(all_data_files):
+    # Load training data
+    print('Load data for', i, 'from', data_file)
+    data = pd.read_csv(os.path.join(data_dir, data_file))
+    cat.train(data.text.values, progress_print=100)
+
+    print('Stats now, after', i)
+    cat.cdb.print_stats()
+
+    # save modelpack
+    cat.create_model_pack(save_dir_path=model_dir, model_pack_name=f"{output_modelpack}_{i}")
+
+# save modelpack - ALL
+cat.create_model_pack(save_dir_path=model_dir, model_pack_name=output_modelpack)
+
