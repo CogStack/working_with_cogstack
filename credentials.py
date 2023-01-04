67be6f29ce523c54204555fe21b06b8c9c4ffca2
antsh3k <antshek@hotmail.com>
2023-01-04 09:44:02 +0000

    remove api auth inputs

--- credentials.py
+++ credentials.py
@@ -5,5 +5,3 @@ hosts = []  # This is a list of your CogStack ElasticSearch instances.
 # These are your login details (either via http_auth or API) Should be in str format
 username = None
 password = None
-api_username = None
-api_password = None
