a2c725f9200e7ffcd2df4c7c9a7a730836ce5880
antsh3k <antshek@hotmail.com>
2023-03-02 17:07:34 +0000

    options for NLM and snomed api auth

--- credentials.py
+++ credentials.py
@@ -1,7 +1,20 @@
-# Any questions on what these details are please contact your local CogStack administrator.
+# CogStack login details
+## Any questions on what these details are please contact your local CogStack administrator.
 
 hosts = []  # This is a list of your CogStack ElasticSearch instances.
 
-# These are your login details (either via http_auth or API) Should be in str format
+## These are your login details (either via http_auth or API) Should be in str format
 username = None
 password = None
+
+
+# NLM authentication
+# The UMLS REST API requires a UMLS account for the authentication described below. 
+# If you do not have a UMLS account, you may apply for a license on the UMLS Terminology Services (UTS) website.
+# https://documentation.uts.nlm.nih.gov/rest/authentication.html
+
+# TODO: add option for UMLS api key auth
+
+
+# SNOMED authentication from international and TRUD
+# TODO add arg for api key auth
