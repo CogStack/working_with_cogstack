5eec38f139d09a25a43552239965545511ca4ffb
mart-r <mart.ratas@gmail.com>
2023-12-12 15:29:51 +0000

    CU-8692my74y / CU-8692kj0tw: Add offline test for full annotation method and meta annotation summary method

--- tests/medcat/evaluate_mct_export/offline_test_mct_analysis.py
+++ tests/medcat/evaluate_mct_export/offline_test_mct_analysis.py
@@ -10,28 +10,37 @@ import sys
 # files in each folder
 # as such, in order to gain access to the relevant module,
 # I'll need to add the path manually
-from .test_mct_analysis import MEDCAT_EVAL_MCT_EXPORT_FOLDER, RESOURCE_DIR
+from .test_mct_analysis import (MEDCAT_EVAL_MCT_EXPORT_FOLDER, RESOURCE_DIR, MCT_EXPORT_JSON_PATH,
+                                BaseMCTExportTests)
 sys.path.append(MEDCAT_EVAL_MCT_EXPORT_FOLDER)
 # and now we can import from mct_analysis
 from mct_analysis import MedcatTrainer_export
 
-import unittest
-
-MCT_EXPORT_JSON_PATH = os.path.join(RESOURCE_DIR, "offline", "MCT_Export_Anthony_v1.5.json")
 
 MODEL_PACK_PATH = os.path.join(RESOURCE_DIR, "offline",
                             #    "trained_model_KCH_cn_only_23_11_27.zip")
                             #    "mc_modelpack_snomed_int_16_mar_2022_new.zip")
                             #    "20230227__kch_gstt_trained_model_494c3717f637bb89.zip")
-                               "anthony_v1.5_model")
+                               "medmen_wstatus_2021_oct.zip")
 
 
-class MCTExportBasicTests(unittest.TestCase):
+class MCTExportBasicTests(BaseMCTExportTests):
     report_path = 'mct_report.xlsx'
 
     @classmethod
     def setUpClass(cls) -> None:
         cls.export = MedcatTrainer_export([MCT_EXPORT_JSON_PATH, ], MODEL_PACK_PATH)
+
+    # these would need a CAT instance
+    def test_can_full_annotation_df(self):
+        full_ann_df = self.export.full_annotation_df()
+        self.assertNonEmptyDataframe(full_ann_df)
+
+    def test_can_meta_anns_concept_summary(self):
+        meta_anns_summary_df = self.export.meta_anns_concept_summary()
+        # this will be empty since I don't think I have anything
+        # of note regarding meta annotations
+        self.assertIsNotNone(meta_anns_summary_df)
 
     def test_generate_report(self):
         self.export.generate_report(path=self.report_path)
