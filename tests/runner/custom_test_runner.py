2bde16f8c51a64586a58bb9055655e11c0b3ec05
mart-r <mart.ratas@gmail.com>
2025-05-13 17:14:28 +0100

    CU-8699049kf: Move test runner to a different folder to fix issues

--- tests/runner/custom_test_runner.py
+++ tests/runner/custom_test_runner.py
@@ -10,44 +10,13 @@ import importlib
 import argparse
 
 # First, ensure medcat compatibility is set up
-try:
-    import medcat
-except ImportError:
-    try:
-        import medcat2
+import medcat
+print("medcat?", medcat, ":", dir(medcat))
+import medcat.cat
+import medcat.vocab
+import medcat.cdb
+print("Loaded medcat")
 
-        # Manually set up the redirection
-        print("Setting up medcat compatibility layer...")
-
-        # Create module for medcat
-        import types
-        medcat_module = types.ModuleType('medcat')
-        sys.modules['medcat'] = medcat_module
-
-        # Copy attributes
-        for attr in dir(medcat2):
-            if not attr.startswith('__'):
-                setattr(medcat_module, attr, getattr(medcat2, attr))
-
-        # Set up submodule proxies
-        class SubmoduleProxy:
-            def __init__(self, target_module_name):
-                self.target_module_name = target_module_name
-            
-            def __getattr__(self, name):
-                return getattr(importlib.import_module(self.target_module_name), name)
-
-        # Add proxies for submodules
-        for module_name in list(sys.modules.keys()):
-            if module_name.startswith('medcat2.'):
-                submodule_name = module_name.replace('medcat2.', 'medcat.', 1)
-            elif module_name == 'medcat2':
-                submodule_name = 'medcat'
-            else:
-                continue
-            sys.modules[submodule_name] = SubmoduleProxy(module_name)  # type: ignore
-    except ImportError:
-        print("Warning: Neither medcat nor medcat2 could be imported")
 
 # Now run the tests
 if __name__ == '__main__':
