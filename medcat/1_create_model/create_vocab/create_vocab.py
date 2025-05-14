5aa4928f76253f2a2d1d24779969cad5525a5c3a
mart-r <mart.ratas@gmail.com>
2025-05-14 13:38:36 +0100

    CU-8699049kf: Make sure to create directory before saving in it (Vocab)

--- medcat/1_create_model/create_vocab/create_vocab.py
+++ medcat/1_create_model/create_vocab/create_vocab.py
@@ -18,5 +18,6 @@ vocab_dir = os.path.join(BASE_PATH, "models", "vocab")
 # embeddings of 300 dimensions is standard
 
 vocab.add_words(os.path.join(vocab_dir, 'vocab_data.txt'), replace=True)
-serialise(AvailableSerialisers.dill, vocab,
-          os.path.join(vocab_dir, "vocab.dat"))
+vocab_folder = os.path.join(vocab_dir, "vocab.dat")
+os.makedirs(vocab_folder, exist_ok=True)
+serialise(AvailableSerialisers.dill, vocab, vocab_folder)
