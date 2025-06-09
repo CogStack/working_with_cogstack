05c8e644c9cf10bd9c53e3d6e3cc3d5147614bb7
mart-r <mart.ratas@gmail.com>
2025-06-09 10:43:03 +0100

    CU-8699049kf: Remove compatibility layer (no longer needed)

--- compatibility_package/setup.py
+++
@@ -1,9 +0,0 @@
-from setuptools import setup
-
-setup(
-    name="medcat",
-    version="2.0.0-beta",
-    description="Compatibility layer for medcat2",
-    py_modules=["medcat"],
-    install_requires=["medcat2"],
-)
